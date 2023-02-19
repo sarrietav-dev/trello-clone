43573298464a5adc08bdd7bb1b15c0447007b976
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-19 17:10:09 -0500

    Add fa icons to the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  faBell = faBell
+  faInfoCircle = faInfoCircle
+
   isOpen = false
 }
